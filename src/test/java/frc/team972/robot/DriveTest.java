6a41fab273be26d9bc9c0a65992c40766650bb24
Jody7 <jody7777pvp@gmail.com>
2019-02-05 01:54:17 -0500

    refactor test

--- src/test/java/frc/team972/robot/DriveTest.java
+++ src/test/java/frc/team972/robot/DriveTest.java
@@ -1,22 +1,25 @@
 package frc.team972.robot;
 
+import frc.team972.robot.util.CoordinateDriveSignal;
+import frc.team972.robot.util.DriveSignal;
+import frc.team972.robot.util.MecanumHelper;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class DriveTest {
 
-    //TODO: write proper unit test
     @Test
     public void testMecanum() {
-//        DriveSignal signal = MecanumHelper.cartesianCalculate(
-//                new CoordinateDriveSignal(1, 5, 20),
-//                30);
-//        //values generated on Lua test-bench
-//        assertEquals(signal.getLeftFront(), 23.196152422707, 0.0001);
-//        assertEquals(signal.getRightFront(), -13.535898384862, 0.0001);
-//        assertEquals(signal.getLeftBack(), 26.464101615138, 0.0001);
-//        assertEquals(signal.getRightBack(), -16.803847577293, 0.0001);
+        DriveSignal signal = MecanumHelper.cartesianCalculate(
+                new CoordinateDriveSignal(1, 0, 0, false),
+                0);
+        /*
+        assertEquals(signal.getLeftFront(), 1, 0.0001);
+        assertEquals(signal.getRightFront(), 1, 0.0001);
+        assertEquals(signal.getLeftBack(), 1, 0.0001);
+        assertEquals(signal.getRightBack(), 1, 0.0001);
+        */
     }
 
 }
