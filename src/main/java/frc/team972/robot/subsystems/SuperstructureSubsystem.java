dff47b4e3686088eab0332d58a76858f8c82ab03
Jody7 <jody7777pvp@gmail.com>
2019-02-16 05:11:02 -0500

    make the state machine a lot better

--- src/main/java/frc/team972/robot/subsystems/SuperstructureSubsystem.java
+++ src/main/java/frc/team972/robot/subsystems/SuperstructureSubsystem.java
@@ -7,13 +7,9 @@ public class SuperstructureSubsystem extends Subsystem {
     IntakeSubsystem intake_ = IntakeSubsystem.getInstance();
 
     enum State {
-        REQUEST_INTAKE_UNSTOW, REQUEST_INTAKE_READY, REQUEST_INTAKE_STOW,
-        STOW_INTAKE,
-        UNSTOW_INTAKE_FROM_STOW, UNSTOW_INTAKE_FROM_READY,
-        READY_INTAKE_FROM_STOW, READY_INTAKE_FROM_UNSTOW,
-        MOVE_ELEVATOR_WRST_FOR_INTAKE_UNSTOW_SAFE, MOVE_ELEVATOR_WRST_FOR_INTAKE_READY_SAFE, MOVE_ELEVATOR_WRST_FOR_INTAKE_STOW_SAFE,
-        CNC_CRASH,
-        WRIST_ELEVATOR_IDLE_INTAKE_UNSTOW, WRIST_ELEVATOR_IDLE_INTAKE_STOW, WRIST_ELEVATOR_IDLE_INTAKE_READY
+        UNSTOW_INTAKE, STOW_INTAKE, READY_INTAKE,
+        SAFE_WRIST_INTAKE,
+        WRIST_ELEVATOR_INTAKE_IDLE
     }
 
     private State state_;
@@ -29,145 +25,48 @@ public class SuperstructureSubsystem extends Subsystem {
         State temp_state = state_;
 
         switch (state_) {
-            case CNC_CRASH: {
-                System.out.println("CNC_CRASH . . . fix this later?");
-                break;
-            }
-
-            case REQUEST_INTAKE_STOW: {
-                if (lastState_ == State.WRIST_ELEVATOR_IDLE_INTAKE_UNSTOW) {
-                    System.out.println("redundant action");
-                    state_ = lastState_;
-                } else if (lastState_ == State.WRIST_ELEVATOR_IDLE_INTAKE_UNSTOW) {
-                    state_ = State.STOW_INTAKE;
-                } else if (lastState_ == State.WRIST_ELEVATOR_IDLE_INTAKE_READY) {
-                    state_ = State.STOW_INTAKE;
-                }
-                break;
-            }
-
-            case REQUEST_INTAKE_UNSTOW: {
-                if (lastState_ == State.WRIST_ELEVATOR_IDLE_INTAKE_STOW) {
-                    state_ = State.UNSTOW_INTAKE_FROM_STOW;
-                } else if (lastState_ == State.WRIST_ELEVATOR_IDLE_INTAKE_UNSTOW) {
-                    System.out.println("redundant action");
-                    state_ = lastState_;
-                } else if (lastState_ == State.WRIST_ELEVATOR_IDLE_INTAKE_READY) {
-                    state_ = State.UNSTOW_INTAKE_FROM_READY;
-                }
-                break;
-            }
-
-            case REQUEST_INTAKE_READY: {
-                if (lastState_ == State.WRIST_ELEVATOR_IDLE_INTAKE_STOW) {
-                    state_ = lastState_.READY_INTAKE_FROM_STOW;
-                } else if (state_ == State.WRIST_ELEVATOR_IDLE_INTAKE_UNSTOW) {
-                    state_ = State.READY_INTAKE_FROM_UNSTOW;
-                } else if (lastState_ == State.WRIST_ELEVATOR_IDLE_INTAKE_READY) {
-                    System.out.println("redundant action");
+            //Moves the wrist/elevator assembly to a safe space for the wrist to move
+            case SAFE_WRIST_INTAKE: {
+                temp_state = lastState_;
+                if(checkIntakeSafeMoveWrist(intake_state, wrist_state)) {
+                    //move the wrist/elevator out of the way in the most efficent manner so that it doesn't hit the intake
+                } else {
+                    //return to the 'last state' before we needed to safe the wrist
+                    temp_state = State.SAFE_WRIST_INTAKE;
                     state_ = lastState_;
                 }
                 break;
             }
-
-            // IDLES ------------------
-            case WRIST_ELEVATOR_IDLE_INTAKE_STOW: {
-
+            case UNSTOW_INTAKE: {
+                if (checkIntakeSafeMoveWrist(intake_state, wrist_state)) {
+                    intake_.setIntake_goal_pos(Constants.kIntakeUnstowPosition);
+                    if (checkTol(intake_state.angle, intake_state.unprofiled_goal_angle, Constants.kIntakeGeneralTolerance)) {
+                        state_ = State.WRIST_ELEVATOR_INTAKE_IDLE;
+                    }
+                } else {
+                    state_ = State.SAFE_WRIST_INTAKE;
+                }
                 break;
             }
-            case WRIST_ELEVATOR_IDLE_INTAKE_UNSTOW: {
-
-                break;
-            }
-            case WRIST_ELEVATOR_IDLE_INTAKE_READY: {
-
-                break;
-            }
-            // ------------------
-
             case STOW_INTAKE: {
                 if (checkIntakeSafeMoveWrist(intake_state, wrist_state)) {
                     intake_.setIntake_goal_pos(Constants.kIntakeStowPosition);
                     if (checkTol(intake_state.angle, intake_state.unprofiled_goal_angle, Constants.kIntakeGeneralTolerance)) {
-                        state_ = State.WRIST_ELEVATOR_IDLE_INTAKE_STOW;
+                        state_ = State.WRIST_ELEVATOR_INTAKE_IDLE;
                     }
-                } else if (lastState_ != State.STOW_INTAKE) {
-                    state_ = State.MOVE_ELEVATOR_WRST_FOR_INTAKE_STOW_SAFE;
                 } else {
-                    state_ = State.CNC_CRASH;
+                    state_ = State.SAFE_WRIST_INTAKE;
                 }
                 break;
             }
-
-            case UNSTOW_INTAKE_FROM_READY: {
-                intake_.setIntake_goal_pos(Constants.kIntakeUnstowPosition);
-                if (checkTol(intake_state.angle, intake_state.unprofiled_goal_angle, Constants.kIntakeGeneralTolerance)) {
-                    state_ = State.WRIST_ELEVATOR_IDLE_INTAKE_UNSTOW;
-                }
-                break;
-            }
-
-            case UNSTOW_INTAKE_FROM_STOW: {
-                if (checkIntakeSafeMoveWrist(intake_state, wrist_state)) {
-                    intake_.setIntake_goal_pos(Constants.kIntakeUnstowPosition);
-                    if (checkTol(intake_state.angle, intake_state.unprofiled_goal_angle, Constants.kIntakeGeneralTolerance)) {
-                        state_ = State.WRIST_ELEVATOR_IDLE_INTAKE_UNSTOW;
-                    }
-                } else if (lastState_ != State.UNSTOW_INTAKE_FROM_STOW) {
-                    state_ = State.MOVE_ELEVATOR_WRST_FOR_INTAKE_UNSTOW_SAFE;
-                } else {
-                    state_ = State.CNC_CRASH;
-                }
-                break;
-            }
-
-            case READY_INTAKE_FROM_STOW: {
-                intake_.setIntake_goal_pos(Constants.kIntakeReadyPosition);
-                if (checkTol(intake_state.angle, intake_state.unprofiled_goal_angle, Constants.kIntakeGeneralTolerance)) {
-                    state_ = State.WRIST_ELEVATOR_IDLE_INTAKE_READY;
-                }
-                break;
-            }
-
-            case READY_INTAKE_FROM_UNSTOW: {
+            case READY_INTAKE: {
                 if (checkIntakeSafeMoveWrist(intake_state, wrist_state)) {
                     intake_.setIntake_goal_pos(Constants.kIntakeReadyPosition);
                     if (checkTol(intake_state.angle, intake_state.unprofiled_goal_angle, Constants.kIntakeGeneralTolerance)) {
-                        state_ = State.WRIST_ELEVATOR_IDLE_INTAKE_READY;
+                        state_ = State.WRIST_ELEVATOR_INTAKE_IDLE;
                     }
-                } else if (lastState_ != State.READY_INTAKE_FROM_UNSTOW) {
-                    state_ = State.MOVE_ELEVATOR_WRST_FOR_INTAKE_READY_SAFE;
                 } else {
-                    state_ = State.CNC_CRASH;
-                }
-                break;
-            }
-
-            // -----------
-
-            case MOVE_ELEVATOR_WRST_FOR_INTAKE_UNSTOW_SAFE: {
-                if (checkIntakeSafeMoveWrist(intake_state, wrist_state)) {
-                    state_ = State.UNSTOW_INTAKE_FROM_STOW;
-                } else {
-                    //TODO: move stuff
-                }
-                break;
-            }
-
-            case MOVE_ELEVATOR_WRST_FOR_INTAKE_READY_SAFE: {
-                if (checkIntakeSafeMoveWrist(intake_state, wrist_state)) {
-                    state_ = State.READY_INTAKE_FROM_STOW;
-                } else {
-                    //TODO: move stuff
-                }
-                break;
-            }
-
-            case MOVE_ELEVATOR_WRST_FOR_INTAKE_STOW_SAFE: {
-                if (checkIntakeSafeMoveWrist(intake_state, wrist_state)) {
-                    state_ = State.STOW_INTAKE;
-                } else {
-                    //TODO: move stuff
+                    state_ = State.SAFE_WRIST_INTAKE;
                 }
                 break;
             }
