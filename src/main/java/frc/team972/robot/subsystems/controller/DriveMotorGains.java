c77e62049f82639bfc7b845c985cf1617a115951
Jody7 <jody7777pvp@gmail.com>
2019-03-08 02:27:45 -0500

    first iteration of fullstate gains for the drive train

--- src/main/java/frc/team972/robot/subsystems/controller/DriveMotorGains.java
+++ src/main/java/frc/team972/robot/subsystems/controller/DriveMotorGains.java
@@ -5,11 +5,11 @@ import jeigen.DenseMatrix;
 public class DriveMotorGains {
 
     public static DenseMatrix A() {
-        return new DenseMatrix("1.0 0.006722413264868324 0.0007031240358986176 ;0.0 0.42536850108185004 0.12327277699260325 ;0.0 0.0 1.0 ;");
+        return new DenseMatrix("1.0 0.00998332154734766 2.7356371472248876e-05 ;0.0 0.996666164973212 0.00546823085576944 ;0.0 0.0 1.0 ;");
     }
 
     public static DenseMatrix Q() {
-        return new DenseMatrix("0.0 0.0 1.9195614552572197e-05 ;0.0 0.0 -0.002647889629283179 ;1.9195614552572197e-05 -0.002647889629283179 0.09846522310415276 ;");
+        return new DenseMatrix("0.0 0.0 0.0 ;0.0 0.0 0.0 ;0.0 0.0 0.0 ;");
     }
 
     public static DenseMatrix C() {
@@ -17,12 +17,10 @@ public class DriveMotorGains {
     }
 
     public static DenseMatrix B() {
-        return new DenseMatrix("0.0007031240358986176 ;0.12327277699260325 ;0.0 ;");
+        return new DenseMatrix("2.7356371472248876e-05 ;0.00546823085576944 ;0.0 ;");
     }
 
-    double dt() {
-        return 0.01;
-    }
+    double dt() { return 0.01; }
 
     public static DenseMatrix D() {
         return new DenseMatrix("0.0 ;");
@@ -33,14 +31,15 @@ public class DriveMotorGains {
     }
 
     public static DenseMatrix K() {
-        return new DenseMatrix("5.443310539518171 0.0690060362816984 1.0 ;");
+        return new DenseMatrix("0.28284271247461923 4.251056247815058 1.0 ;");
     }
 
     public static DenseMatrix Kff() {
-        return new DenseMatrix("0.0 8.112091123411645 0.0 ;");
+        return new DenseMatrix("0.0 182.8745029930323 0.0 ;");
     }
 
     public static DenseMatrix L() {
-        return new DenseMatrix("1.3664965832424907 ;50.01213955122777 ;264.3961819792072 ;");
+        return new DenseMatrix("1.6069546155857644 ;2.9251868219507626 ;0.013254539702527842 ;");
     }
 }
+
