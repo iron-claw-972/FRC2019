d482e2d8e278a357eee9cf7ccc904d9de183ecaa
Jody7 <jody7777pvp@gmail.com>
2019-03-11 04:36:44 -0400

    gains to 6.0 kg

--- src/main/java/frc/team972/robot/subsystems/controller/ElevatorGains.java
+++ src/main/java/frc/team972/robot/subsystems/controller/ElevatorGains.java
@@ -4,14 +4,16 @@ import jeigen.DenseMatrix;
 
 import jeigen.DenseMatrix;
 
+import jeigen.DenseMatrix;
+
 public class ElevatorGains {
 
     public static DenseMatrix A() {
-        return new DenseMatrix("1.0 0.008586527925338995 0.00040682978894687906 ;0.0 0.7312971983847448 0.07733884951125544 ;0.0 0.0 1.0 ;");
+        return new DenseMatrix("1.0 0.006936444461149615 0.00088176178054063 ;0.0 0.4573353524946653 0.1561913729823607 ;0.0 0.0 1.0 ;");
     }
 
     public static DenseMatrix Q() {
-        return new DenseMatrix("0.0 0.0 2.032950102467479e-06 ;0.0 0.0 -0.00045818092672426865 ;2.032950102467479e-06 -0.00045818092672426865 0.02490308746973273 ;");
+        return new DenseMatrix("0.0 0.0 5.773257551509884e-06 ;0.0 0.0 -0.0008501498907233547 ;5.773257551509884e-06 -0.0008501498907233547 0.024419288254708838 ;");
     }
 
     public static DenseMatrix C() {
@@ -19,7 +21,7 @@ public class ElevatorGains {
     }
 
     public static DenseMatrix B() {
-        return new DenseMatrix("0.00040682978894687906 ;0.07733884951125544 ;0.0 ;");
+        return new DenseMatrix("0.00088176178054063 ;0.1561913729823607 ;0.0 ;");
     }
 
     double dt() { return 0.01; }
@@ -33,15 +35,15 @@ public class ElevatorGains {
     }
 
     public static DenseMatrix K() {
-        return new DenseMatrix("9.000000000000009 1.7246430136911568 1.0 ;");
+        return new DenseMatrix("9.000000000000018 1.608017811015691 1.0 ;");
     }
 
     public static DenseMatrix Kff() {
-        return new DenseMatrix("0.0 12.930112179318957 0.0 ;");
+        return new DenseMatrix("0.0 6.402402264002981 0.0 ;");
     }
 
     public static DenseMatrix L() {
-        return new DenseMatrix("0.2505513604929526 ;0.010886165465222256 ;0.025664825456869576 ;");
+        return new DenseMatrix("0.25086821528185516 ;0.01724394190780159 ;0.058917131499979895 ;");
     }
 }
 
